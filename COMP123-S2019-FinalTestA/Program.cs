b5ca8667f0f504e5398a293ddde09f7fe3509a7d
301044399 <301044399@PR-E216-30>
2019-08-09 09:45:19 -0400

    Added Master form to Views Folder

--- COMP123-S2019-FinalTestA/Program.cs
+++ COMP123-S2019-FinalTestA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using COMP123_S2019_FinalTestA.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace COMP123_S2019_FinalTestA
 {
     static class Program
     {
+        //this is temporary
+        public static MasterForm masterform;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,8 @@ namespace COMP123_S2019_FinalTestA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            masterform = new MasterForm();
+            Application.Run(masterform);
         }
     }
 }
